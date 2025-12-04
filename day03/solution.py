62193ca9b2213b351415a1895ccee967507bfb02
Oskar Blom <xosblo91@gmail.com>
2025-12-04 21:19:46 +0100

    dag tre

--- day03/solution.py
+++ day03/solution.py
@@ -1,21 +1,31 @@
 def main():
     data = read()
     sol1(data)
+    sol2(data)
 
 
 def sol1(data):
     sum_joltage = 0
 
     for row in data:
-        sum_joltage += int(optimize(row))
+        sum_joltage += int(optimize_v1(row))
 
     print(sum_joltage)
 
-def optimize(row):
-    highest = 0
-    highest_str=0
-    for index,battery in enumerate(row):
-        for i in range(index,len(row)):
+
+def sol2(data):
+    sum_joltage = 0
+
+    for row in data:
+        sum_joltage += int(optimize_v2(row, 12))
+
+    print(sum_joltage)
+
+
+def optimize_v1(row):
+    highest_str = 0
+    for index, battery in enumerate(row):
+        for i in range(index, len(row)):
             if index == i:
                 continue
             temp_str = str(battery) + str(row[i])
@@ -24,17 +34,33 @@ def optimize(row):
 
     return highest_str
 
-def read():
-    data_2d_int=[]
 
-    with open("sample.txt", 'r') as f:
+def optimize_v2(row, battery_count):
+    result = []
+    deletions_allowed = len(row) - battery_count
+
+    for battery in row:
+        while len(result) > 0 and battery > result[-1] and deletions_allowed > 0:
+            result.pop()
+            deletions_allowed -= 1
+
+        result.append(battery)
+
+    return "".join(map(str, result[:battery_count]))
+
+
+def read():
+    data = []
+
+    with open("input.txt", "r") as f:
         for line in f:
             clean_line = line.strip()
-            if clean_line: # Ensure we don't process empty lines
+            if clean_line:
                 row_of_ints = [int(char) for char in clean_line]
-                data_2d_int.append(row_of_ints)
+                data.append(row_of_ints)
 
-    return data_2d_int
+    return data
+
 
 if __name__ == "__main__":
     main()
