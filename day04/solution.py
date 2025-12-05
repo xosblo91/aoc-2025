079d0f70f5376a981089fb61fa41b0c6ddb9d443
Oskar Blom <xosblo91@gmail.com>
2025-12-05 17:16:51 +0100

    dag 4 del 2

--- day04/solution.py
+++ day04/solution.py
@@ -1,10 +1,12 @@
 def main():
     data = read()
-    sol1(data)
-
-
-def sol1(grid):
     adjacent = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+
+    sol1(data, adjacent)
+    sol2(data, adjacent)
+
+
+def sol1(grid, adjacent):
     accessible = 0
 
     for x, row in enumerate(grid):
@@ -21,6 +23,36 @@ def sol1(grid):
                 accessible += 1
 
     print(accessible)
+
+
+def sol2(grid, adjacent):
+    removed = 0
+
+    keep_removing = True
+    while keep_removing:
+        grid, keep_removing = remover(grid, adjacent)
+        if keep_removing:
+            removed += 1
+
+    print(removed)
+
+
+def remover(grid, adjacent):
+    for x, row in enumerate(grid):
+        for y, col in enumerate(row):
+            if col != "@":
+                continue
+            count = 0
+            for ax, ay in adjacent:
+                if 0 <= x + ax < len(grid) and 0 <= y + ay < len(row):
+                    if grid[x + ax][y + ay] == "@":
+                        count += 1
+
+            if count < 4:
+                grid[x][y] = "."
+                return grid, True
+
+    return grid, False
 
 
 def read():
